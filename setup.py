8d6dcdcc6dff792604404fa6eb0f218fd006b536
RoseLohr <rosemarielohr@gmx.de>
2018-03-13 19:39:55 +0100

    sys.setdefaultencoding('utf8')

--- setup.py
+++ setup.py
@@ -29,6 +29,9 @@ import sys
 import subprocess
 import textwrap
 
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
 
 if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[:2] < (3, 4):
     raise RuntimeError("Python version 2.7 or >= 3.4 required.")
